7cd8ef4a5dcc790c181d3ae6768bad518b8465ef
Edward <edwardthupane80@gmail.com>
2024-09-05 15:34:34 +0200

    juska

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,6 +1,4 @@
 //CORE_REFERENCE_IMPORTS
-//CORE_REFERENCE_IMPORT-behaviorSubject-SSD_SERVICE_ID_sd_UwSGXhKxotyTXMGp
-import { behaviorSubject as SSD_SERVICE_ID_sd_UwSGXhKxotyTXMGp } from './sd-services/Observables/behaviorSubject';
 //CORE_REFERENCE_IMPORT-api-SSD_SERVICE_ID_sd_6s2lCJ5QRBD6nGIT
 import { api as SSD_SERVICE_ID_sd_6s2lCJ5QRBD6nGIT } from './sd-services/Nicholas/api';
 //CORE_REFERENCE_IMPORT-validate-SSD_SERVICE_ID_sd_SmKGdfPXeNu37Xdl
@@ -11,9 +9,7 @@ import { messages as SSD_SERVICE_ID_sd_hebxVU7E4NfTBifC } from './sd-services/me
 import { Auth as SSD_SERVICE_ID_sd_qcsHvGUKUKU7RTSL } from './sd-services/Auth';
 
 export const UserRoutes = [
-    //CORE_REFERENCE_PUSH_TO_ROUTE_ARRAY
-//CORE_REFERENCE_PUSH_TO_ROUTE_ARRAY-behaviorSubject-SSD_SERVICE_ID_sd_UwSGXhKxotyTXMGp
-SSD_SERVICE_ID_sd_UwSGXhKxotyTXMGp,
+//CORE_REFERENCE_PUSH_TO_ROUTE_ARRAY
 //CORE_REFERENCE_PUSH_TO_ROUTE_ARRAY-api-SSD_SERVICE_ID_sd_6s2lCJ5QRBD6nGIT
 SSD_SERVICE_ID_sd_6s2lCJ5QRBD6nGIT,
 //CORE_REFERENCE_PUSH_TO_ROUTE_ARRAY-validate-SSD_SERVICE_ID_sd_SmKGdfPXeNu37Xdl
